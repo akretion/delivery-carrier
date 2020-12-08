b711907463c9f3fc8432f93167074738d0e99b0c
Florian da Costa <florian.dacosta@akretion.com>
2020-12-08 19:12:05 +0100

    Split readme

--- delivery_roulier_option/__manifest__.py
+++ delivery_roulier_option/__manifest__.py
@@ -1,6 +1,3 @@
-# © 2016 Raphael REVERDY <raphael.reverdy@akretion.com>
-#        David BEAL <david.beal@akretion.com>
-#        Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
