3e44a6fede5e173484e7cd82f101fc28b916f916
Benoit <benoit.guillot@akretion.com>
2023-11-28 14:57:11 +0100

    [IMP] delivery_carrier_b2c: black, isort, prettier

--- delivery_carrier_b2c/__manifest__.py
+++ delivery_carrier_b2c/__manifest__.py
@@ -1,23 +1,20 @@
-# -*- coding: utf-8 -*-
 #    Author: Chafique DELLI <chafique.delli@akretion.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'Delivery Carrier Business To Customer',
-    'version': '10.0.0.1.0',
-    'author': "Akretion,Odoo Community Association (OCA)",
-    'maintainer': 'Akretion',
-    'category': '',
-    'depends': [
+    "name": "Delivery Carrier Business To Customer",
+    "version": "10.0.0.1.0",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainer": "Akretion",
+    "category": "",
+    "depends": [],
+    "website": "https://github.com/OCA/delivery-carrier",
+    "data": [
+        "partner_view.xml",
     ],
-
-    'website': 'http://www.akretion.com/',
-    'data': [
-        'partner_view.xml',
-    ],
-    'tests': [],
-    'installable': True,
-    'auto_install': False,
-    'license': 'AGPL-3',
-    'application': False,
+    "tests": [],
+    "installable": True,
+    "auto_install": False,
+    "license": "AGPL-3",
+    "application": False,
 }
--- delivery_carrier_b2c/partner.py
+++ delivery_carrier_b2c/partner.py
@@ -1,18 +1,20 @@
-# -*- coding: utf-8 -*-
 #    Author: Chafique DELLI <chafique.delli@akretion.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    use_b2c_info = fields.Boolean('Advanced address',
-                                  help="Display additional information for "
-                                       "home delivery (b2c)")
-    door_code = fields.Char('Door Code')
-    door_code2 = fields.Char('Door Code 2')
-    intercom = fields.Char('Intercom', help="Informations for Intercom such as"
-                           " name or number on the intercom")
+    use_b2c_info = fields.Boolean(
+        "Advanced address",
+        help="Display additional information for " "home delivery (b2c)",
+    )
+    door_code = fields.Char("Door Code")
+    door_code2 = fields.Char("Door Code 2")
+    intercom = fields.Char(
+        "Intercom",
+        help="Informations for Intercom such as" " name or number on the intercom",
+    )
