4a9e465f1956001cab1ca51bd0e9ecd0c60cacbd
Benoît <benoit.guillot@akretion.com.br>
2024-11-02 10:02:08 +0100

    use final shipping partner for delivery pickup

--- delivery_roulier_laposte_fr_dropoff_site/models/stock_picking.py
+++ delivery_roulier_laposte_fr_dropoff_site/models/stock_picking.py
@@ -6,11 +6,8 @@ from odoo import models
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    def _roulier_convert_address(self, partner):
-        address = super()._roulier_convert_address(partner)
-        # mobile is mandatory for dropoff site delivery
-        if self.final_shipping_partner_id and self.final_shipping_partner_id.mobile:
-            address["mobilePhone"] = self.final_shipping_partner_id.mobile.replace(
-                " ", ""
-            )
-        return address
+    def _laposte_fr_get_receiver(self, package=None):
+        receiver = super()._roulier_get_receiver(package=package)
+        if self.final_shipping_partner_id:
+            receiver = self.final_shipping_partner_id
+        return receiver
