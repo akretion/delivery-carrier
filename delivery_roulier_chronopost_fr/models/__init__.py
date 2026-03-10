d840ef15867789ee3494b9d23c0ef311ebc89bf1
chafique-delli <chafique.delli@akretion.com>
2026-03-10 14:03:11 +0100

    [18.0][MIG] delivery_roulier_chronopost_fr

---
+++ delivery_roulier_chronopost_fr/models/__init__.py
@@ -0,0 +1,5 @@
+from . import delivery_carrier
+from . import stock_picking
+from . import stock_quant_package
+from . import carrier_account
+from . import stock_package_type
---
+++ delivery_roulier_chronopost_fr/models/stock_package_type.py
@@ -0,0 +1,9 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class StockPackageType(models.Model):
+    _inherit = "stock.package.type"
+
+    package_carrier_type = fields.Selection(selection_add=[("chronopost_fr", "CHRONOPOST")])
