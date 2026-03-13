3716a0f83f54df9cc8d893a60594a2916bb64e4f
Olivier Nibart <olivier.nibart@akretion.com.br>
2026-03-13 10:42:47 +0100

    [FIX] fix licence

--- delivery_roulier_gls_fr/models/res_partner.py
+++ delivery_roulier_gls_fr/models/res_partner.py
@@ -1,4 +1,5 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# @author Olivier Nibart <olivier.nibart@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
 
